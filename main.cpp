ea0f7b104e4f0a9689d7a13b39740149ed70f451
gamonite <spiritdragon83@yahoo.com>
2019-02-17 11:36:11 -0500

    fixed random

--- main.cpp
+++ main.cpp
@@ -6,20 +6,28 @@ using namespace std;
 
 int main()
 {
-	int attack = rand() % 100 + 1;
-	int defense = rand() % 100 + 1;
-	int mattack = rand() % 100 + 1;
-	int mdefense = rand() % 100 + 1;
-	int agility = rand() % 100 + 1;
-	int luck = rand() % 100 + 1;
-	int mhp = rand() % 100 + 1;
-	int mmp = rand() % 100 + 1;
+	srand(time(0));
+
+	int attack = rand() % (100 + 1);
+	int defense = rand() % (100 + 1);
+	int mattack = rand() % (100 + 1);
+	int mdefense = rand() % (100 + 1);
+	int agility = rand() % (100 + 1);
+	int luck = rand() % (50 + 1);
+	int mhp = rand() % (100 + 1);
+	int mmp = rand() % (100 + 1);
+
 
 	cout << "Attack is " << attack << endl;
+
 	cout << "Defense is " << defense << endl;
+
 	cout << "M-Attack is " << mattack << endl;
+
 	cout << "M-Defense is " << mdefense << endl;
+
 	cout << "Agility is " << agility << endl;
+
 	cout << "Luck is " << luck << endl;
 
 	return 0;
