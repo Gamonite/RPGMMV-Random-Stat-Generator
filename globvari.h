921a04dba965cf40e18993e484d94d885e5788f0
Gamonite <47681585+Gamonite@users.noreply.github.com>
2020-05-13 20:35:33 -0500

    Update globvari.h

--- globvari.h
+++ globvari.h
@@ -8,3 +8,27 @@ using namespace std;
 extern string mtype;
 
 extern string mmagic;
+
+extern int itype;
+
+extern int imagic;
+
+extern int mtier;
+
+extern int mult;
+
+extern int attack;
+
+extern int defense;
+
+extern int mattack;
+
+extern int mdefense;
+
+extern int agility;
+
+extern int luck;
+
+extern int mhp;
+
+extern int mmp;
